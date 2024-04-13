8853eb50179f9133fa22f45bd86f334c27c3737d
kake <kaisla.ketola@metropolia.fi>
2024-04-13 20:08:05 +0300

    Refactor cat-controller.js to use async/await for getCat, getCatById, and postCat functions

--- src/api/controllers/cat-controller.js
+++ src/api/controllers/cat-controller.js
@@ -1,11 +1,11 @@
 import {addCat, findCatById, listAllCats} from '../models/cat-model.js';
 
-const getCat = (req, res) => {
-  res.json(listAllCats());
+const getCat = async (req, res) => {
+  res.json(await listAllCats());
 };
 
-const getCatById = (req, res) => {
-  const cat = findCatById(req.params.id);
+const getCatById = async (req, res) => {
+  const cat = await findCatById(req.params.id);
   if (cat) {
     res.json(cat);
   } else {
@@ -13,46 +13,12 @@ const getCatById = (req, res) => {
   }
 };
 
-const postCat = (req, res) => {
-  const result = addCat(req.body);
-  if (result.cat_id) {
-    res.status(201);
-    res.json({message: 'New cat added.', result});
-  } else {
-    res.sendStatus(400);
-  }
-};
-
-const putCat = (req, res) => {
-  // not implemented in this example, this is future homework
-  res.sendStatus(200);
-};
-
-const deleteCat = (req, res) => {
-  // not implemented in this example, this is future homework
-  res.sendStatus(200);
-};
-
-export {getCat, getCatById, postCat, putCat, deleteCat};
-
-import {addCat, findCatById, listAllCats} from '../models/cat-model.js';
-
-const getCat = (req, res) => {
-  res.json(listAllCats());
-};
-
-const getCatById = (req, res) => {
-  const cat = findCatById(req.params.id);
-  if (cat) {
-    res.json(cat);
-  } else {
-    res.sendStatus(404);
-  }
-};
-
-const postCat = (req, res) => {
+const postCat = async (req, res) => {
   // console.log('postCat', req.body);
-  const result = addCat(req.body);
+  // console.log("req", req)
+  // console.log("file", req.file)
+
+  const result = await addCat(req.body, req.file);
   if (result.cat_id) {
     res.status(201);
     res.json({message: 'New cat added.', result});
@@ -61,9 +27,13 @@ const postCat = (req, res) => {
   }
 };
 
-const putCat = (req, res) => {
-  // not implemented in this example, this is future homework
-  res.sendStatus(200);
+const putCat = async (req, res) => {
+  const result = await modifyCat(req.body, req.params.id, res.locals.user);
+  if (!result) {
+    res.sendStatus(400);
+    return;
+  }
+  res.json(result);
 };
 
 const deleteCat = (req, res) => {
